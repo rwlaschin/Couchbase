3dcd3b476d38ef5ba5380f740fa302314112d36a
Robert Wlaschin <raw77_m@yahoo.com>
2014-09-20 15:29:02 -0700

    Adding tests for commands
    add, set, get, delete
    . removed file that was symlink on linux

--- Test.hx
+++ Test.hx
@@ -21,14 +21,16 @@ class Test{
 	}
 
 	static function CouchbaseTest() {
-		/*var cb = new Couchbase(['localhost'],"user","password","default",false);
-		trace(CouchbaseConst.COUCHBASE_SUCCESS);
-		var cb = new Couchbase(['localhost:8091'],"user","password","default",false);
-		trace(CouchbaseConst.COUCHBASE_SUCCESS);
-		var cb = new Couchbase(['127.0.0.1'],"user","password","default",false);
-		trace(CouchbaseConst.COUCHBASE_SUCCESS);
-		var cb = new Couchbase(['127.0.0.1:8091'],"user","password","default",false);
-		trace(CouchbaseConst.COUCHBASE_SUCCESS);*/
+		/*
+			var cb = new Couchbase(['localhost'],"user","password","default",false);
+			trace(CouchbaseConst.COUCHBASE_SUCCESS);
+			var cb = new Couchbase(['localhost:8091'],"user","password","default",false);
+			trace(CouchbaseConst.COUCHBASE_SUCCESS);
+			var cb = new Couchbase(['127.0.0.1'],"user","password","default",false);
+			trace(CouchbaseConst.COUCHBASE_SUCCESS);
+			var cb = new Couchbase(['127.0.0.1:8091'],"user","password","default",false);
+			trace(CouchbaseConst.COUCHBASE_SUCCESS);
+		*/
 	}
 
 	static function CouchbaseSocketTest() {
@@ -36,57 +38,91 @@ class Test{
 		var con:CouchbaseSocket;
 		var resp;
 
+		try { // Test, good host name
+			con = new CouchbaseSocket("localhost");
+			con.send('stats','');
+			var resp = con.read();
+			trace( resp );
+		} catch (e:String) {
+			trace( "Expected Exception - " + Std.string( e ) );
+		}
+
+		try { // Test, good ip 
+			con = new CouchbaseSocket("127.0.0.1");
+			con.send('stats','');
+			var resp = con.read();
+			trace( resp );
+		} catch (e:String) {
+			trace( "Expected Exception - " + Std.string( e ) );
+		}
+
+		try { // Test, bad host
+			con = new CouchbaseSocket("foonuggets");
+			con.send('stats','');
+			var resp = con.read();
+			trace( resp );
+		} catch (e:String) {
+			trace( "Expected Exception - " + Std.string( e ) );
+		}
+
+		try { // Test, bad port
+			con = new CouchbaseSocket("localhost",10001);
+			con.send('stats','');
+			var resp = con.read();
+			trace( resp );
+		} catch (e:String) {
+			trace( "Expected Exception - " + Std.string( e ) );
+		}
+
 		try {
+			var data:Array<Dynamic> = [
+				{ key : "mynewstring",
+				  value : "This is the data I'm storing", 
+				  result : ["STORED","This is the data I'm storing"],
+				  cmd : [ 'add', 'get' ] 
+				},
+				{ key : "mynewstring",
+				  value : "This is the data I'm storing", 
+				  result : ["NOT_STORED","NOT_STORED","This is the data I'm storing",""], 
+				  cmd : [ 'add', 'set', 'get', 'remove' ]
+				}
+			];
+
 			// command list: add, set, get, delete
 			//               replace, cas, gets(for cas value)
+
 			con = new CouchbaseSocket("localhost",11211);
-/*
-			// just a string
-			con.send('add','mynewstring','This is the data I\'m storing');
-			var resp = con.read();
-			trace( "Expected - STORED, Got - " + Std.string(resp) );
-
-			con.send('add','mynewstring','This is the data I\'m storing');
-			var resp = con.read();
-			trace( "Expected - NOT_STORED, Got - " + Std.string(resp) );
-
-			con.send('set','mynewstring','This is the data I\'m storing');
-			var resp = con.read();
-			trace( Std.string(resp) );
-
-			con.send('set','mynewstring1','This is the data I\'m storing');
-			var resp = con.read();
-			trace( Std.string(resp) );
-*/
-			con.send('get','mynewstring');
-			var resp = con.read();
-			trace( Std.string(resp) );
-/*
-			con.send('get','notexisting');
-			var resp = con.read();
-			trace( Std.string(resp) );
-
-			con.send('delete','mynewstring');
-			var resp = con.read();
-			trace( Std.string(resp) );
-
-			con.send('delete','mynewstring1');
-			var resp = con.read();
-			trace( Std.string(resp) );
-
-			con.send('delete','notexisting');
-			var resp = con.read();
-			trace( Std.string(resp) );
-*/
+			for( i in 0...data.length ) {
+				var testInfo:Dynamic = data[i];
+				var cmds:Array<String> = testInfo.cmd;
+				var results:Array<String> = testInfo.result;
+				for(j in 0...cmds.length ) {
+					var cmd:String = cmds[j];
+					var result:String = results[j];
+					trace( "Test <"+ testInfo.key +"> - cmd <"+ cmd +">" );
+					switch( cmd ) {
+						case "add": con.send(cmd,testInfo.key,testInfo.value);
+						case "set": con.send(cmd,testInfo.key,testInfo.value);
+						case "replace": con.send(cmd,testInfo.key,testInfo.value);
+						case "get": con.send(cmd,testInfo.key);
+						case "delete": con.send(cmd,testInfo.key);
+					}
+					var response = con.read();
+					trace( "Expected - " + Std.string( result ) );
+					trace( "Recieved - " + Std.string( response ) );
+					trace( ( result == response ) ? "Passed" : "Failed" );
+				}
+			}
+			
 		} catch ( e:Dynamic ) {
 			trace( Std.string(e) );
 		}
-		// send command
-		
 	}
 
 	static function MemcacheTest() {
-		/*var cb = new Memcache(['localhost'],"user","password","default",false);*/
+		/*
+			var cb = new Memcache(['localhost'],"user","password","default",false);
+		*/
 	}
 
 	static function MemcacheSocketTest() {
